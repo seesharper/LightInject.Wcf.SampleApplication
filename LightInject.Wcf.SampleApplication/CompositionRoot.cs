a1c530c0b0c47e2dd2a377783f8c3fc5603a51a4
Bernhard Richter <bernhard.richter@gmail.com>
2016-04-21 11:06:07 +0200

    Added metadata

--- LightInject.Wcf.SampleApplication/CompositionRoot.cs
+++ LightInject.Wcf.SampleApplication/CompositionRoot.cs
@@ -1,10 +1,20 @@
 ﻿namespace LightInject.Wcf.SampleApplication
 {
+    using System.ServiceModel.Description;
+
     public class CompositionRoot : ICompositionRoot
     {
         public void Compose(IServiceRegistry serviceRegistry)
         {
             serviceRegistry.Register<IService, Service>("LightInject-Wcf-SampleApplication-IService");
+            serviceRegistry.Register<IServiceBehavior>(f => CreateServiceMetaBehavior());
+        }
+
+        private ServiceMetadataBehavior CreateServiceMetaBehavior()
+        {
+            var serviceMetaBehavior = new ServiceMetadataBehavior();
+            serviceMetaBehavior.HttpGetEnabled = true;
+            return serviceMetaBehavior;
         }
     }
 }
